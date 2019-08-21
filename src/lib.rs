8618b3482d52a49230d3bfd7e1597bd566f37b25
Markus Westerlind <markus.westerlind@distilnetworks.com>
2019-08-21 14:20:02 +0200

    refactor: Remove unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -281,9 +281,7 @@ where
                 }
                 self.retry = self.retry.saturating_add(1);
 
-                if err.kind() == ErrorKind::ExtensionError {
-                    let error_code = err.extension_error_code().unwrap();
-
+                if let Some(error_code) = err.extension_error_code() {
                     if error_code == "MOVED" || error_code == "ASK" {
                         // Refresh slots and request again.
                         self.info.excludes.clear();
