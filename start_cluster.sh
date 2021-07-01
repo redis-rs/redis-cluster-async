22f1a0bedaac0721aa1a42ab2720aea421f7c978
Markus Westerlind <markus.westerlind@distilnetworks.com>
2021-07-01 16:56:13 +0200

    Update to a working redis

--- start_cluster.sh
+++ start_cluster.sh
@@ -7,4 +7,4 @@ exec docker run \
     --name redis-cluster \
     -e "IP=127.0.0.1" \
     -p 7000:7000 -p 7001:7001 -p 7002:7002 -p 7003:7003 -p 7004:7004 -p 7005:7005 \
-    grokzen/redis-cluster:latest
+    grokzen/redis-cluster:6.0.0
